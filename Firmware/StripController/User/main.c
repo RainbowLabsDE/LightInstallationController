c9244f7b684658526ee67bc8a718441553806c6a
LeoDJ <leandro.spaeth@gmail.com>
2023-02-16 00:19:31 +0100

    add PWM example

--- Firmware/StripController/User/main.c
+++ Firmware/StripController/User/main.c
@@ -22,9 +22,7 @@
 
 #include "debug.h"
 
-
 /* Global define */
-
 
 /* Global Variable */
 vu8 val;
@@ -36,9 +34,8 @@ vu8 val;
  *
  * @return  none
  */
-void USARTx_CFG(void)
-{
-    GPIO_InitTypeDef  GPIO_InitStructure = {0};
+void USARTx_CFG(void) {
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
     USART_InitTypeDef USART_InitStructure = {0};
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
@@ -64,32 +61,77 @@ void USARTx_CFG(void)
 }
 
 /*********************************************************************
+ * @fn      TIM1_OutCompare_Init
+ *
+ * @brief   Initializes TIM1 output compare.
+ *
+ * @param   arr - the period value.
+ *          psc - the prescaler value.
+ *          ccp - the pulse value.
+ *
+ * @return  none
+ */
+void TIM1_PWMOut_Init(u16 arr, u16 psc, u16 ccp) {
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+    TIM_OCInitTypeDef TIM_OCInitStructure = {0};
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_TIM1, ENABLE);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    TIM_TimeBaseInitStructure.TIM_Period = arr;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = psc;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitStructure);
+
+#if (PWM_MODE == PWM_MODE1)
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+
+#elif (PWM_MODE == PWM_MODE2)
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
+
+#endif
+
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = ccp;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+
+    TIM_CtrlPWMOutputs(TIM1, ENABLE);
+    TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Disable);
+    TIM_ARRPreloadConfig(TIM1, ENABLE);
+    TIM_Cmd(TIM1, ENABLE);
+}
+
+/*********************************************************************
  * @fn      main
  *
  * @brief   Main program.
  *
  * @return  none
  */
-int main(void)
-{
+int main(void) {
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     Delay_Init();
     USART_Printf_Init(115200);
-    printf("SystemClk:%d\r\n",SystemCoreClock);
+    printf("SystemClk:%d\r\n", SystemCoreClock);
+    TIM1_PWMOut_Init(100, 48000 - 1, 50);
 
     USARTx_CFG();
 
-    while(1)
-    {
+    while (1) {
 
-        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET)
-        {
+        while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET) {
             /* waiting for receiving finish */
         }
         val = (USART_ReceiveData(USART1));
         USART_SendData(USART1, ~val);
-        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
-        {
+        while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET) {
             /* waiting for sending finish */
         }
     }
